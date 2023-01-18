f17be210b7cf850f3622de525942fa64fa6ae019
IvanSlivenko <ivan8822@ulr.net>
2023-01-18 21:33:03 +0200

    etap-18-01-2023-21-32

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// import apiServis from './js/api-servis'
+import validatePassword from './js/validate-password';
+// import {
+//     fetchAllUsers,
+//     fetchUserById,
+//     updateUserById,
+//     x as value,
+//     y as name
+// } from './js/api-servis'
+import * as apiServise from './js/api-servis';
+
+// console.log('x',x);
+// console.log('y',y);
+console.log(apiServise);
+
